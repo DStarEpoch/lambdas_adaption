576f9ba62f662325dcc8ec53e40f4a248a4f8be1
yeqing <yuyeqing@T14-YUYEQ>
2023-12-18 16:59:03 +0800

    fix toy model parameters

--- toy_model.py
+++ toy_model.py
@@ -2,72 +2,138 @@
 import tqdm
 import numpy as np
 import pandas as pd
+from typing import List
+from pathlib import Path
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
 from alchemlyb.estimators import MBAR
 from util.ising_model import IsingModel
 from alchemlyb.visualisation import plot_mbar_overlap_matrix
 from util.calc_partial_overlap import calc_partial_overlap_matrix
 
 
-state_num = 40
-betas = 1 / np.linspace(1.8, 2.0, state_num)
-# betas = 1 / np.linspace(1.53, 3.28, state_num)
+STATE_NUM = 40
+BETAS = 1 / np.linspace(1.6, 2.4, STATE_NUM)
+# BETAS = 1 / np.linspace(1.53, 3.28, STATE_NUM)
+RELAX_STEPS = 1000
 N_STEPS = 2000
-EXCHANGE_STEPS = 20
-SAMPLE_STEPS = 10
-N = 40
-models = [IsingModel(N=N, beta=beta) for beta in betas]
-E = [[] for _ in betas]
-config_traj_list = [[] for _ in betas]
-exchange_flag = True
-for step in tqdm.tqdm(range(N_STEPS), desc="sampling"):
-    for model_idx in range(len(models)):
-        model = models[model_idx]
-        model.mcmove()
-        E[model_idx].append(model.dimless_energy)
+SAMPLE_STEPS = 50
+N = 40  # N x N grid for the Ising model
+PROCESSES = 8
+figure_path = Path("./figures")
+figure_path.mkdir(parents=True, exist_ok=True)
 
-    if step % SAMPLE_STEPS == (SAMPLE_STEPS - 1):
-        for model_idx in range(len(models)):
-            config_traj_list[model_idx].append(models[model_idx].config)
-    # if step % EXCHANGE_STEPS == 0:
-    #     for model_idx in range(len(models)):
-    #         config_traj_list[model_idx].append(models[model_idx].config)
-    #     exchange_flag = exchange_flag ^ True
-    #     exchange_status = ""
-    #     for k in range(state_num):
-    #         if (exchange_flag and k % 2 == 0) or (not exchange_flag and k % 2 == 1):
-    #             flag = models[k].swap(models[(k + 1) % state_num])
-    #             exchange_status += f"{k}x" if flag else f"{k} "
-    #         else:
-    #             exchange_status += f"{k} "
-    #     print(f"exchange:\n{exchange_status}")
+
+class ParaForwardContext:
+    obj: IsingModel
+    idx: int
+    forward_steps: int
+    sample_steps: int
+
+    def __init__(self, idx: int, obj: IsingModel, forward_steps: int, sample_steps: int):
+        self.idx = idx
+        self.obj = obj
+        self.forward_steps = forward_steps
+        self.sample_steps = sample_steps
+
+
+def parallel_forward(context: ParaForwardContext):
+    obj = context.obj
+    E_list = list()
+    config_list = list()
+    for i in range(context.forward_steps):
+        obj.mcmove()
+        E_list.append(obj.dimless_energy)
+        if i % context.sample_steps == context.sample_steps - 1:
+            config_list.append(obj.config)
+    return {"idx": context.idx, "obj": obj, "E_list": E_list, "config_list": config_list}
+
+
+models = [IsingModel(N=N, beta=beta) for beta in BETAS]
+# relax
+params = [ParaForwardContext(idx, m, RELAX_STEPS, SAMPLE_STEPS) for idx, m in enumerate(models)]
+if PROCESSES > 1:
+    with Pool(PROCESSES) as pool:
+        result = list(tqdm.tqdm(pool.imap(parallel_forward, params), total=len(params), desc="relaxing"))
+        result.sort(key=lambda r: r["idx"])
+        for x in result:
+            models[x["idx"]] = x["obj"]
+else:
+    for param in tqdm.tqdm(params, total=len(params), desc="relaxing"):
+        result = parallel_forward(param)
+        models[result["idx"]] = result["obj"]
+
+# sampling run
+params = [ParaForwardContext(idx, m, N_STEPS, SAMPLE_STEPS) for idx, m in enumerate(models)]
+E = [[] for _ in BETAS]
+config_traj_list = [[] for _ in BETAS]
+if PROCESSES > 1:
+    with Pool(PROCESSES) as pool:
+        result = list(tqdm.tqdm(pool.imap(parallel_forward, params), total=len(params), desc="sampling"))
+        result.sort(key=lambda r: r["idx"])
+        for x in result:
+            E[x["idx"]] = x["E_list"]
+            config_traj_list[x["idx"]] = x["config_list"]
+            models[x["idx"]] = x["obj"]
+else:
+    for param in tqdm.tqdm(params, total=len(params), desc="sampling"):
+        result = parallel_forward(param)
+        E[result["idx"]] = result["E_list"]
+        config_traj_list[result["idx"]] = result["config_list"]
+        models[x["idx"]] = x["obj"]
 
 color_list = ["red", "green", "blue", "yellow", "grey", "purple", "orange", "pink", "cyan", "brown"]
-for model_idx in range(len(models)):
+for model_idx in range(len(E)):
     plt.hist(E[model_idx], bins=50, alpha=0.5,
              color=color_list[model_idx % len(color_list)],
              label=f"beta_{model_idx}", density=True)
 plt.gca().set(title='Frequency Histogram', ylabel='Frequency')
-plt.show()
+plt.savefig(figure_path / f"Ising_energy_distribution_{STATE_NUM}_{N_STEPS}_{N}.png")
+# plt.show()
 
-org_u_nks = []
-for o in range(state_num):
-    print(f"collect u_nks state: {o}")
+
+# collect u_nks for MBAR estimation
+class ParaCollectContext:
+    idx: int
+    config_list: List[np.ndarray]
+    model_list: List[IsingModel]
+
+    def __init__(self, idx: int, config_list: list, model_list: list):
+        self.idx = idx
+        self.model_list = model_list
+        self.config_list = config_list
+
+
+def parallel_calc_u_nk(context: ParaCollectContext):
     u_k = list()
-    for ro in range(state_num):
+    for model in context.model_list:
         p_on_j = list()
-        for cfg in config_traj_list[ro]:
-            p_on_j.append(models[o].dimlessEnergyOnConfig(cfg))
+        for cfg in context.config_list:
+            p_on_j.append(model.dimlessEnergyOnConfig(cfg))
         u_k.append(p_on_j)
-    u_k = pd.DataFrame(np.asarray(u_k).T, columns=[f"state_{i}" for i in range(state_num)])
+    u_k = pd.DataFrame(np.asarray(u_k).T, columns=[f"state_{i}" for i in range(len(context.model_list))])
     # rename window list
     u_k.columns = [f'{i}' for i in range(len(u_k.columns))]
     # add a column to df for using groupby
-    u_k['lambda'] = f"lambda_{o}"
-    u_k['window'] = f"{o}"
+    u_k['lambda'] = f"lambda_{context.idx}"
+    u_k['window'] = f"{context.idx}"
     # set lambda index for later groupby
     u_k = u_k.set_index(['lambda', 'window'])
-    org_u_nks.append(u_k)
+    return {"idx": context.idx, "u_k": u_k}
+
+
+org_u_nks = []
+params = [ParaCollectContext(idx, config_list, models) for idx, config_list in enumerate(config_traj_list)]
+if PROCESSES > 1:
+    with Pool(PROCESSES) as pool:
+        result = list(tqdm.tqdm(pool.imap(parallel_calc_u_nk, params), total=len(params), desc="collect u_nks"))
+        result.sort(key=lambda r: r["idx"])
+        for x in result:
+            org_u_nks.append(x["u_k"])
+else:
+    for param in tqdm.tqdm(params, total=len(params), desc="collect u_nks"):
+        result = parallel_calc_u_nk(param)
+        org_u_nks.append(result["u_k"])
 
 mbar_estimator = MBAR(method="L-BFGS-B").fit(pd.concat([u_nk for u_nk in org_u_nks]))
 org_overlap_matrix = mbar_estimator.overlap_matrix
@@ -76,9 +142,8 @@ for i in range(len(mbar_estimator.delta_f_) - 1):
     f_k.append(mbar_estimator.delta_f_.iloc[i, i+1] + f_k[i])
     print(f"{i} -> {i+1}: {mbar_estimator.delta_f_.iloc[i, i+1]}, f_k: {f_k[i+1]}")
 ax = plot_mbar_overlap_matrix(mbar_estimator.overlap_matrix)
-plt.show()
-
-
+ax.figure.savefig(figure_path / f"Ising_overlap_matrix_{STATE_NUM}_{N_STEPS}_{N}.png")
+# plt.show()
 
 plot_data = {
     "x": [],
@@ -94,7 +159,7 @@ for j in range(estimate_start_lambda_idx+1, estimate_end_lambda_idx):
     remove_lambda_list = list(range(estimate_start_lambda_idx+1, j))
     # remain_lambda_list = [l for l in range(STATE_NUM) if l not in
     #                       list(range(estimate_start_lambda_idx+1, j)) + [1, 3, 18]]
-    remain_lambda_list = [l for l in range(state_num) if l not in remove_lambda_list]
+    remain_lambda_list = [l for l in range(STATE_NUM) if l not in remove_lambda_list]
     lc = -1
     for o in remain_lambda_list:
         lc += 1
@@ -119,7 +184,7 @@ for j in range(estimate_start_lambda_idx+1, estimate_end_lambda_idx):
     # C2 = sum([org_overlap_matrix[i, k] * org_overlap_matrix[j, k] /
     #           sum([org_overlap_matrix[l, k] for l in remain_lambda_list]) for k in range(STATE_NUM)])
     C1 = sum([partial_overlap_matrix[k][i][j] for k in remain_lambda_list])
-    C2 = sum([partial_overlap_matrix[k][i][j] for k in range(state_num)])
+    C2 = sum([partial_overlap_matrix[k][i][j] for k in range(STATE_NUM)])
     print("\nC1: {}, C2: {}".format(C1, C2))
     C = C2 / C1
     # C = np.exp(1 - C2 / C1)
@@ -136,4 +201,5 @@ h1 = plt.plot(range(len(plot_data["estimate"])), plot_data["estimate"], color="r
 h2 = plt.plot(range(len(plot_data["real"])), plot_data["real"], color="blue", marker="^")
 plt.legend(handles=[h1[0], h2[0]], labels=["estimate", "real"], loc="best")
 plt.xticks(range(len(plot_data["x"])), plot_data["x"], rotation=45)
-plt.show()
+plt.savefig(figure_path / f"Ising_estimate_{STATE_NUM}_{N_STEPS}_{N}.png")
+# plt.show()
